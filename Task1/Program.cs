73fc68948f3fde70405262e7f58e0227002f65a5
Max Bogatyrskiy <tesla.maksim@mail.ru>
2024-03-23 01:13:10 +0400

    Решил первую задачу

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число :");
+string input = Console.ReadLine();
+int int_input = Convert.ToInt32(input);
+
+if (int_input % 7 == 0 && int_input % 23 == 0)
+{
+    Console.WriteLine("Да, число кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Нет, число не кратно 7 и 23");
+}
